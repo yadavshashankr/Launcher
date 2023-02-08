e6377ab03ff7ee5c004e7134296776ca06cbc08f
Shashank Yadav <shashank.yadav@ignitesol.com>
2023-02-09 03:19:06 +0530

    Initial Commit.

---
+++ app/src/main/java/com/example/launcher/ui/LauncherActivity.java
@@ -0,0 +1,57 @@
+package com.example.launcher.ui;
+
+import static com.example.launcher.utils.AppUtils.getAllApps;
+import static com.example.launcher.utils.AppUtils.isMyAppLauncherDefault;
+import static com.example.launcher.utils.AppUtils.resetPreferredLauncherAndOpenChooser;
+import static com.example.launcher.utils.AppUtils.traversed;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+import com.example.launcher.adapter.AppsDrawerAdapter;
+
+/**
+ * This is a simple Activity, whose work is to set the list of launcher apps,
+ * creating a list by adding/modifying its ui parameters programmatically,
+ * passing appropriate parameters to view and its elements
+ * and finally passing the created View to the container of BaseActivity.
+ *
+ * This activity being our Launcher, also checks for the current launcher app, if it
+ * is this app then it loads normally or else it asks for Launcher selection.
+ */
+
+public class LauncherActivity extends BaseActivity {
+    private AppsDrawerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(!isMyAppLauncherDefault(getApplicationContext()) && !traversed){
+            traversed = true;
+            resetPreferredLauncherAndOpenChooser(getApplicationContext());
+        }
+
+        setLauncherList();
+        launch(initViews());
+    }
+
+    public RelativeLayout initViews() {
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        RecyclerView recyclerView = new RecyclerView(this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutParams(new RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT, RecyclerView.LayoutParams.MATCH_PARENT));
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+        relativeLayout.addView(recyclerView);
+        return relativeLayout;
+    }
+
+    private void setLauncherList(){
+        adapter = new AppsDrawerAdapter(this, getAllApps(getApplicationContext()));
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/launcher/ui/TempActivity.java
@@ -0,0 +1,22 @@
+package com.example.launcher.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class TempActivity extends AppCompatActivity {
+
+    /**
+     * This activity is used to hold the dialog for Launcher selection.
+     * This activity's default state is disabled.
+     * It is enabled for few moments until it shows the dialog and
+     * switches to LauncherActivity.
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, LauncherActivity.class));
+    }
+}
